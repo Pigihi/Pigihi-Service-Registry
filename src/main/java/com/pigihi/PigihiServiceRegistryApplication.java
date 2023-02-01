20d56746f006741fb7b3f18f4daf6f521d63fefe
Ashish Sam <ashishsamtgeorge@mca.ajce.in>
2023-02-01 22:22:13 +0530

    Configure Eureka Server

--- src/main/java/com/pigihi/PigihiServiceRegistryApplication.java
+++ src/main/java/com/pigihi/PigihiServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package com.pigihi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PigihiServiceRegistryApplication {
 
 	public static void main(String[] args) {
